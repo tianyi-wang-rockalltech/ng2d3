1f010083bcfa5d3e44065cdc19a836fe54eaf7a4
Tianyi Wang <tianyi.wang@rockall.com>
2016-10-30 22:48:27 +0000

    new build

--- src/bar-chart/bar-horizontal.component.ts
+++ src/bar-chart/bar-horizontal.component.ts
@@ -59,7 +59,7 @@ import d3 from '../d3';
 [attr.y2]="line.y2 + 50 + i * 20"
 [attr.stroke]="line.color" stroke-dasharray="5, 5" />
 
-<text x="0" y="0">{{ mainLabel }}</text>
+<text x="0" y="0" font-size="13" font-weight="bold" [attr.fill="scheme[0]"]>{{ mainLabel }}</text>
 <text *ngFor="let line of lines; let i=index; trackBy:trackBy"
 font-weight="bold" font-size="13"
 [attr.x]="line.x2 - 150" [attr.y]="line.y2 + 60 + i * 20"
